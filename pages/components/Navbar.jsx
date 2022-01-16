f25235764ca76a95c25d9a2b0db39fcace391441
Jeremy <than2314@gmail.com>
2022-01-16 15:24:10 +0700

    style: 💄 make responsive

--- pages/components/Navbar.jsx
+++ pages/components/Navbar.jsx
@@ -1,17 +1,22 @@
 const Navbar = () => {
 	return (
-		<nav className="h-[4em] bg-[#2a2e35] flex items-center justify-between px-8">
-			<div className="text-white font-bold text-2xl">Jeremy Status</div>
-			<ul className="flex space-x-9 text-white font-bold text-xltext-[#b2becd]">
-				<li className="cursor-default text-[#b2becd]">Contact</li>
-				<li>
-					<a href="https://github.com/pp3times/jerestatus" rel="noopener noreferrer" target="_blank">
-						github
-					</a>
-				</li>
-				{/* <li>Language</li> */}
-			</ul>
-		</nav>
+		<div>
+			<nav className="md:flex hidden h-[4em] bg-[#2a2e35] items-center justify-between px-8">
+				<div className="text-white font-bold text-xl md:text-2xl">Jeremy Status</div>
+				<ul className="flex space-x-9 text-white font-bold text-xl text-[#b2becd]">
+					<li className="cursor-default text-[#b2becd]">Contact</li>
+					<li>
+						<a href="https://github.com/pp3times/jerestatus" rel="noopener noreferrer" target="_blank">
+							github
+						</a>
+					</li>
+					{/* <li>Language</li> */}
+				</ul>
+			</nav>
+			<nav className="md:hidden h-[4em] bg-[#2a2e35] flex items-center justify-center px-8">
+				<div className="text-white font-bold text-xl md:text-2xl">Jeremy Status</div>
+			</nav>
+		</div>
 	)
 }
 
--- pages/components/Sleep.jsx
+++ pages/components/Sleep.jsx
@@ -3,8 +3,8 @@ import Date from './Date'
 
 const Sleep = () => {
 	return (
-		<div className="h-[84vh] relative bg-[#342b4a] space-y-8">
-			<div className='absolute text-white text-9xl flex items-center justify-center flex-col z-50 w-full h-full backdrop-opacity-10 backdrop-invert bg-[#12181b]/80'>
+		<div className="lg:h-[84vh] h-[86vh] relative bg-[#342b4a] space-y-8">
+			<div className='absolute text-white text-7xl md:text-9xl flex items-center justify-center flex-col z-50 w-full h-full backdrop-opacity-10 backdrop-invert bg-[#12181b]/80'>
 				<div className='breathing flex items-center justify-center flex-col space-y-8'>
 					Sleeping
 					<span className="text-white text-xl">
